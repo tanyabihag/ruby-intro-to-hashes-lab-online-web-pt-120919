08853f2f23d60b02268c0565e0add0ec4914da70
Tanya Bihag <tbihag@gmail.com>
2019-12-20 21:59:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ end
 def monopoly_with_second_tier
 	monopoly = {:railroads => {
 	  :pieces => 4,
-	  :names => {}
+	  :names => {},
 	  :rent_in_dollars => {}
 	}
 	  
@@ -30,7 +30,7 @@ end
 def monopoly_with_third_tier
 	monopoly = {:railroads => {
 	  :pieces => 4,
-	  :names => {}
+	  :names => {},
 	  :rent_in_dollars => {}
 	}
 	  
